9b32f6583cf12390c26fee1b8af5eea4d50df62f
guedera <guilhermegg5@al.insper.edu.br>
2024-10-09 20:17:31 -0300

    maskcreator

--- mascaracreator.py
+++ mascaracreator.py
@@ -10,17 +10,31 @@ def calcular_mascara_hsv(rgb):
     h, s, v = hsv_cor[0][0]
     print(f"HSV da cor de entrada: H={h}, S={s}, V={v}")
 
-    # Definir limites para H
+    # Definir margens para H
     margem_h = 10
     lower_h = max(0, h - margem_h)
     upper_h = min(179, h + margem_h)
 
-    # Definir limites fixos para S e V
-    lower_s = 50
-    upper_s = 230
+    # Calcular margens dinâmicas para S e V (porcentagem do valor original)
+    percentual_margem_s = 0.4  # Você pode ajustar esse valor conforme necessário
+    percentual_margem_v = 0.4  # Você pode ajustar esse valor conforme necessário
 
-    lower_v = 150
-    upper_v = 255
+    margem_s = s * percentual_margem_s
+    margem_v = v * percentual_margem_v
+
+    # Calcular limites inferior e superior para S
+    lower_s = max(0, s - margem_s)
+    upper_s = min(255, s + margem_s)
+
+    # Calcular limites inferior e superior para V
+    lower_v = max(0, v - margem_v)
+    upper_v = min(255, v + margem_v)
+
+    # Converter valores para inteiros
+    lower_s = int(lower_s)
+    upper_s = int(upper_s)
+    lower_v = int(lower_v)
+    upper_v = int(upper_v)
 
     mascara_inferior = np.array([lower_h, lower_s, lower_v], dtype=np.uint8)
     mascara_superior = np.array([upper_h, upper_s, upper_v], dtype=np.uint8)
